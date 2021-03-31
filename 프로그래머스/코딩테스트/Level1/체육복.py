8e9424db845bf175ef43a5f7c6c71c02faa56843
김혁 <kimhyuk@gimhyeog-ui-MacBookPro.local>
2021-03-31 14:45:47 +0900

    체육복

---
+++ 프로그래머스/코딩테스트/Level1/체육복.py
@@ -0,0 +1,18 @@
+def solution(n, lost, reverse):
+    _reverse = [r for r in reverse if r not in lost]
+    _lost = [i for i in lost if i not in reverse]
+
+    for r in _reverse:
+        f = r-1
+        b = r+1
+        if f in _lost:
+            _lost.remove(f)
+        elif b in _lost:
+            _lost.remove(b)
+    return n - len(_lost)
+
+
+n = 5
+lost = [2, 4]
+reverse = [1, 3, 5]
+print(solution(n, lost, reverse))
