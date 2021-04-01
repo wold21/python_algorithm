fb9312b90432f23e13a7e934c660e3bb591f45c9
wold21 <wold2180@gmail.com>
2021-04-01 16:33:03 +0900

    복기 2016년

--- 프로그래머스/코딩테스트/Level1/복기/2016년.py
+++ 프로그래머스/코딩테스트/Level1/복기/2016년.py
@@ -1,5 +1,11 @@
 def solution(a, b):
-    weekOfDay = []
+    weekOfDay = ["FRI", "SAT", "SUN", "MON", "TUE",
+                 "WED", "THU"]  # 2016년 1월1일은 금요일부터 시작
+    month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  # 2016년 달의 일수
+    something = (sum(month[:a-1]) - b) % 7
+    # a값까지 일수를 더한 후 진행된 b까지 뺀다.
+    # 그리고 요일 수 만큼 나머지 연산을 한다.
+    return(weekOfDay[something])  # 나머지 값을 요일의 인덱스 값으로 보냄.
 
 
 a = 5
